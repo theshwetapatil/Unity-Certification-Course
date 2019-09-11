6fa0bd416250d83d9993460cedca5953127509ef
theshwetapatil <theshwetapatil@gmail.com>
2019-09-11 13:13:01 +0530

    Created tags

---
+++ Project Management/Project Management/Assets/Scripts/TagBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TagBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var shapes = GameObject.FindGameObjectsWithTag("Shape");
+		var respawnPT = GameObject.FindGameObjectWithTag("Respwan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
