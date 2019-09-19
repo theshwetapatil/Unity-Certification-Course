4f2375f4e2e386c38d32f7756db76db968260eb0
Shweta Patil <theshwetapatil@gmail.com>
2019-09-19 16:27:04 +0530

    Added Script

---
+++ Code/Code/Assets/Scripts/ConsoleWelcomer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsoleWelcomer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Welcome");
+        print("beginning...");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
