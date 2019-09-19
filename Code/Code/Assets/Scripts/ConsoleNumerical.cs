586cf07a3990556e524d653fa6a69e21b68f0642
Shweta Patil <theshwetapatil@gmail.com>
2019-09-19 17:26:31 +0530

    Added methods

--- Code/Code/Assets/Scripts/ConsoleNumerical.cs
+++ Code/Code/Assets/Scripts/ConsoleNumerical.cs
@@ -7,14 +7,25 @@ public class ConsoleNumerical : MonoBehaviour {
 	void Start () {
         int INT = 2;
         float FLOAT = 2.0f;
+        float FFLOAT = 4.0f;
         float sum = INT + FLOAT;
-        float avg = sum / 2;
+        float avg = Average(FLOAT, FFLOAT);
+        float sqr = Square(Square(FLOAT));
         print("INT = " + INT);
         print("FLOAT = " + FLOAT);
         print("sum = " + sum);
         print("avg = " + avg);
+        print("sqr = " + sqr);
 	}
 	
+    float Square(float num)
+    {
+        return (num * num);
+    }
+    float Average(float num1, float num2)
+    {
+        return ((num1+num2)/2);
+    }
 	// Update is called once per frame
 	void Update () {
 	
