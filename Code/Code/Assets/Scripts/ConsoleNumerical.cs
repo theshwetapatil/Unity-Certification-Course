dd4518b330b624b5b1cd9a088405e87b8e579c9d
Shweta Patil <theshwetapatil@gmail.com>
2019-09-19 16:43:35 +0530

    Added numericals

---
+++ Code/Code/Assets/Scripts/ConsoleNumerical.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsoleNumerical : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int INT = 2;
+        float FLOAT = 2.0f;
+        float sum = INT + FLOAT;
+        float avg = sum / 2;
+        print("INT = " + INT);
+        print("FLOAT = " + FLOAT);
+        print("sum = " + sum);
+        print("avg = " + avg);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
