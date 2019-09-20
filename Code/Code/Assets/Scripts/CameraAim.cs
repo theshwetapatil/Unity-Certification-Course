1ce585fbb900545f65a854795f77fc2d1a9a4cf5
Shweta Patil <theshwetapatil@gmail.com>
2019-09-20 15:40:32 +0530

    Added Camera API: ScreenPointToArray

---
+++ Code/Code/Assets/Scripts/CameraAim.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Debug.DrawRay(transform.position, Vector3.forward, Color.red, 10);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Camera cam = GetComponent<Camera>();
+        Vector3 mousePos = Input.mousePosition;
+        Ray aimRay = cam.ScreenPointToRay(mousePos);
+        Debug.DrawRay(aimRay.origin, aimRay.direction, Color.red);
+    }
+}
