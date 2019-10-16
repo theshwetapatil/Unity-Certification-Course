fd6e94e0a0c609c495c6e3c21cb4d24cc9cae1ab
Shweta Patil <theshwetapatil@gmail.com>
2019-10-16 12:49:05 +0530

    Added Ads

---
+++ Services/Services/Assets/Scripts/AdsScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ShowAd();
+	}
+
+    public void ShowAd()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+}
