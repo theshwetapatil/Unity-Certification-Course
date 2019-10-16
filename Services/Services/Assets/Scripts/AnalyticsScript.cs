0e248dddd344d77361718f7bcf2b82ad20258caf
Shweta Patil <theshwetapatil@gmail.com>
2019-10-16 14:00:20 +0530

    Added Analytics

---
+++ Services/Services/Assets/Scripts/AnalyticsScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class AnalyticsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Gender gender = Gender.Female;
+        Analytics.SetUserGender(gender);
+
+        int birthYear = 2014;
+        Analytics.SetUserBirthYear(birthYear);
+
+        LevelUp();
+	}
+	
+    void LevelUp()
+    {
+        Analytics.Transaction("levelUp", 1, "INR", null, null);
+
+        Analytics.CustomEvent("levelUp", new Dictionary<string, object>
+        {
+            { "newLevgel", 2 },
+            { "trigger", "purchase" },
+        });
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
